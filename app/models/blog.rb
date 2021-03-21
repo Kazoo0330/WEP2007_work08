01283b780612fd4afb7ae32e6c0a9bbffbc80b15
hiroyuki.nakagawa0325@gmail.com <hiroyuki.nakagawa0325@gmail.com>
2021-03-21 17:53:54 +0900

    バリデーションの追加

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,6 @@
 class Blog < ApplicationRecord
   mount_uploader :image, ImageUploader
+  validates :title, presence: true, length: { maximum: 10 }
+  validates :content, presence: true, length: { maximum: 300 }
+  validates :image, presence: true
 end
